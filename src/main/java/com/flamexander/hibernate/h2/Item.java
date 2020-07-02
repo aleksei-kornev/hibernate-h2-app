c13841f28a6badc3b9bae8918640c8128a0fae00
kornev <aakor23@gmail.com>
2020-07-02 16:36:58 +0300

    HW has finished.

--- src/main/java/com/flamexander/hibernate/h2/Item.java
+++ src/main/java/com/flamexander/hibernate/h2/Item.java
@@ -1,6 +1,7 @@
 package com.flamexander.hibernate.h2;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "items")
@@ -13,14 +14,46 @@ public class Item {
     @Column(name = "title")
     private String title;
 
+    @Column(name = "price")
+    private int price;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_items",
+            joinColumns = @JoinColumn(name = "item_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> users;
+
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public String getTitle () {
+        return this.title;
     }
 
     public Item() {
     }
 
+    public Item(String title, int price) {
+        this.price = price;
+        this.title = title;
+    }
+
     public void print() {
-        System.out.println("Item id = " + id + "; title = " + title);
+        System.out.println("Item id = " + id + "; title = " + title + "; price = " + price);
     }
 }
--- src/main/java/com/flamexander/hibernate/h2/MainApp.java
+++ src/main/java/com/flamexander/hibernate/h2/MainApp.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.jws.soap.SOAPBinding;
+
 public class MainApp {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
@@ -17,22 +19,56 @@ public class MainApp {
             User user = session.get(User.class, 1L);
             user.print();
 
-            User user2 = new User();
-            user2.setName("Bob2");
-            session.save(user2);
-            user2.print();
+            for (int i = 0; i < 10; i++) {
+                User userNew = new User();
+                userNew.setName("Bob" + i);
+                session.save(userNew);
+                userNew.print();
+            }
 
             Item itemBox = session.get(Item.class, 1L);
             itemBox.print();
 
             for (int i = 0; i < 10; i++) {
                 Item item = new Item();
-                item.setTitle("Sphere" + i);
+                item.setTitle("Cube" + i);
+                item.setPrice(i * 10);
                 session.save(item);
                 item.print();
             }
 
             session.getTransaction().commit();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            User userToPrint = session.get(User.class, 1L);
+            userToPrint.print();
+            System.out.println("Этот пользователь покупал товары: ");
+            for (Item itm : userToPrint.getItems()) {
+                System.out.println(itm.getTitle());
+            }
+
+            Item itemToPrint = session.get(Item.class, 3L);
+            itemToPrint.print();
+            System.out.println("Этот товар покупали пользователи: ");
+            for (User usr : itemToPrint.getUsers()) {
+                System.out.println(usr.getName());
+            }
+            session.getTransaction().commit();
+
+            System.out.println("Удаление пользователя #1");
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.delete(session.get(User.class, 1L));
+            session.getTransaction().commit();
+
+            System.out.println("Удаление товара #1");
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.delete(session.get(Item.class, 1L));
+            session.getTransaction().commit();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
